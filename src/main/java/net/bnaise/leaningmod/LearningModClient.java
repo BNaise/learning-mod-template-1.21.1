84841386f4fbf20057d9c74d7a16afd1e9bf4311
B. Naise <forsec8890@gmail.com>
2025-05-08 16:28:19 +0530

    Initial commit

---
+++ src/main/java/net/bnaise/leaningmod/LearningModClient.java
@@ -0,0 +1,14 @@
+package net.bnaise.leaningmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LearningModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+
+
+    }
+
+}
